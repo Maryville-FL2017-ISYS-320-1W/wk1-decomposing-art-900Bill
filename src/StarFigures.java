503df41a26bb578fc97076b3e0b65c292f0edd5d
900Bill <Apvuera@gmail.com>
2017-11-03 09:12:53 -0500

    Starfigures

--- src/StarFigures.java
+++ src/StarFigures.java
@@ -1,16 +1,31 @@
 /*
   	ISYS 320
-  	Name(s):
-  	Date: 
+  	Name(s): Antoine Payne	
+  	Date: 11/2/2017
 */
 
 // 4. Your pseudocode algorithm for how to break down the figure
 
 
 public class StarFigures {
-
 	public static void main(String[] args) {
-
-	}
-
+        int number = 8;
+ 
+        for (int i = 0; i < number; ++i) {
+            for (int j = 0; j <= i; ++j) {
+                System.out.print("*");
+            }
+ 
+            if (i != number - 1) {
+                System.out.print(" ");
+            } else {
+                System.out.print(" *");
+            }
+            for (int j = 0; j <= i; ++j) {
+                System.out.print("*");
+            }
+ 
+            System.out.println();
+        }
+    }
 }
