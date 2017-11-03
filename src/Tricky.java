d3107b08f4980a842901d7c6e306fb7db94ac9df
900Bill <Apvuera@gmail.com>
2017-11-02 20:28:40 -0500

    TrickyJava
    Prediction and output

--- src/Tricky.java
+++ src/Tricky.java
@@ -1,15 +1,29 @@
 /*
   	ISYS 320
-  	Name(s):
-  	Date: 
+  	Name(s): Antoine Payne
+  	Date: 11/2/2017
 */
 
-// 1. Your predicted output 
- 
+// 1. Your predicted output the console will display the text in a funky text
 
 public class Tricky {
-
+    public static void main(String[] args) {
+        message1();
+        message2();
+        System.out.println("My book arrives in a day, hope that will help me");
+    }
+    
+    public static void message1() {
+        System.out.println("My prediction was wrong");
+    }
+    
+    public static void message2() {
+        System.out.println("Tricky rearranges the messages");
+        message1();
+        System.out.println("This is all a guess");
+    }
 }
+
 
 // 3. Were you correct? Explain any differences
  
